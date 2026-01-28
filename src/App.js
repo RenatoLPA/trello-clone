35d4a3b9acb86a71b89554e3a739e2358c8b06e0
RenatoLPA <renatolucaspandrade@gmail.com>
2026-01-28 02:01:20 -0300

    changes2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,118 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const inicialItems = [
+  {id:"1", content:"Conteúdo 1"},
+  {id:"2", content:"Conteúdo 2"},
+  {id:"3", content:"Conteúdo 3"},
+]
+
+const inicialColumns = [
+  {
+    name: "Atribuidos",
+    id: "123",
+    items: inicialItems,
+  },
+  {
+    name: "Testes",
+    id: "456",
+    items: [],
+  },
+  {
+    name: "Aprovados",
+    id: "789",
+    items: [],
+  },
+]
 
 function App() {
+
+  const [columns, setColumns] = useState(inicialColumns);
+
+  const onDragEnd = (result) => {
+    console.log(result);
+    //var sourceColumItems = columns[0].items;
+    var sourceColumItems = [];
+    var destinationColumnsItems = [];
+    var draggedItem = {};
+
+    var sourceColumnId = 0;
+    var destinationColumnId = 0;
+
+    for (var i in columns) {
+      if(columns[i].id == result.source.droppableId) {
+        sourceColumItems = columns[i].items
+        sourceColumnId = i
+      } else if (columns[i].id == result.destination.droppableId) {
+        destinationColumnsItems = columns[i].items
+        destinationColumnId = i
+      }
+    }
+
+    for(var i in sourceColumItems) {
+      if(sourceColumItems[i].id == result.draggableId) {
+        draggedItem = sourceColumItems[i];
+      }
+    }
+
+    //Excluir o objeto arrastado
+    var filteredSourceColumnItems = sourceColumItems.filter( 
+      (item) => item.id != result.draggableId);
+
+    //Adicionar o mesmo na nova posição
+    if(result.source.droppableId == result.destination.droppableId) {
+      filteredSourceColumnItems.splice(result.destination.index, 0, draggedItem);
+
+      //Mudar o state
+      var columnsCopy = JSON.parse(JSON.stringify(columns));
+      columnsCopy[sourceColumnId].items = filteredSourceColumnItems;
+      setColumns(columnsCopy);
+    } else {
+      destinationColumnsItems.splice(result.destination.index, 0, draggedItem);
+      //Mudar o state
+      var columnsCopy = JSON.parse(JSON.stringify(columns));
+      columnsCopy[sourceColumnId].items = filteredSourceColumnItems;
+      columnsCopy[destinationColumnId].items = destinationColumnsItems;
+      setColumns(columnsCopy);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Trello Clone
-        </p>
-      </header>
+    <div style={{display:"flex", justifyContent:"center"}}>
+      <DragDropContext onDragEnd={onDragEnd}>
+        {columns.map((column) => (
+          <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+            <h1>{column.name}</h1>
+            <Droppable droppableId={column.id} key={column.id}>
+              {(provided) => (
+                <div 
+                  ref={provided.innerRef}
+                  style={{backgroundColor:"lightblue", width:250, height: 500, padding: 10, margin: 10}}
+                >  
+                          {column.items.map((item, index)=>(
+                            <Draggable draggableId={item.id} index={index} key={item.id}>
+                              {(provided) => (
+                                <div 
+                                  {...provided.dragHandleProps}
+                                  {...provided.draggableProps}
+                                  ref={provided.innerRef} 
+                                  style={{backgroundColor:"gray", height: 40, marginBottom: 1,...provided.draggableProps.style}}
+                                  >
+                                    {item.content}
+                                </div>
+                              )}
+                            </Draggable>  
+                          ))}
+                          {provided.placeholder}                   
+
+                  </div>
+              )}
+            </Droppable> 
+          </div>
+        ))}
+      </DragDropContext>
     </div>
   );
 }
